a9fb99dd07841935c99690ae07da519c3236ee2d
Filebb <fil.cavallari@gmail.com>
2025-12-30 09:31:03 +0000

    Add section headers

--- R/2015/05.R
+++ R/2015/05.R
@@ -1,23 +1,30 @@
+# Day 5: Doesn't He Have Intern-Elves For This?
+# https://adventofcode.com/2015/day/5
+
+# Setup ----
 library(tidyverse)
 library(cli)
 
-input <- read_csv("inputs/2015/05.txt", col_names = "original")
+# Load Data ----
+input <- read_lines("inputs/2015/05.txt")
 
-nice <- input |>
+# Part 1 - Check strings with old rules ----
+nice <- tibble(string = input) |>
     mutate(
-        three_more_vowels = str_count(original, "[aeiou]") >= 3,
-        one_more_double = str_detect(original, "([a-z])\\1"),
-        not_ab_cd_pq_xy = !str_detect(original, "ab|cd|pq|xy"),
+        three_more_vowels = str_count(string, "[aeiou]") >= 3,
+        one_more_double = str_detect(string, "([a-z])\\1"),
+        not_ab_cd_pq_xy = !str_detect(string, "ab|cd|pq|xy"),
         nice = three_more_vowels & one_more_double & not_ab_cd_pq_xy
     )
 
 total_nice_strings <- sum(nice$nice)
 cli_alert_success("Total nice strings (old rules): {total_nice_strings}")
 
-nice_new <- input |>
+# Part 2 - Check strings with new rules ----
+nice_new <- tibble(string = input) |>
     mutate(
-        repeating_pair = str_detect(original, "([a-z]{2}).*\\1"),
-        repeat_with_gap = str_detect(original, "([a-z]).\\1"),
+        repeating_pair = str_detect(string, "([a-z]{2}).*\\1"),
+        repeat_with_gap = str_detect(string, "([a-z]).\\1"),
         nice_new = repeating_pair & repeat_with_gap
     )
 
