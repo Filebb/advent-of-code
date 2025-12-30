c3a3997d0523bfd34e402ffe17edc3f773770c98
Filebb <fil.cavallari@gmail.com>
2025-12-30 01:24:51 +0000

    Add section headers and improve organization

--- R/2015/02.R
+++ R/2015/02.R
@@ -1,20 +1,30 @@
+# Day 2: I Was Told There Would Be No Math
+# https://adventofcode.com/2015/day/2
+
+# Setup ----
 library(tidyverse)
 library(cli)
 
-input <- read_csv("inputs/2015/02.txt", col_names = "original")
+# Load Data ----
+input <- read_lines("inputs/2015/02.txt")
 
-paper <- input |>
-    separate_wider_delim(cols = original,
+# Pre-processing ----
+paper <- tibble(present_dimensions = input) |>
+    separate_wider_delim(cols = present_dimensions,
                          names = c("l", "w", "h"),
                          delim = "x",
                          cols_remove = FALSE) |>
-    relocate(original, l, w, h) |>
-    mutate(across(c(l, w, h), as.integer)) |>
+    relocate(present_dimensions, l, w, h) |>
+    mutate(across(c(l, w, h), as.integer))
+
+# Part 1 - Calculate wrapping paper needed ----
+paper <- paper |>
     mutate(paper_needed = 2L * (l*w + w*h + h*l) + pmin(l*w, w*h, h*l))
 
 total_paper_needed <- sum(paper$paper_needed)
 cli_alert_success("Total paper needed: {total_paper_needed} sq ft")
 
+# Part 2 - Calculate ribbon needed ----
 paper <- paper |>
     mutate(ribbon_needed = 2L * pmin(l+w, l+h, w+h) + l*w*h)
 
